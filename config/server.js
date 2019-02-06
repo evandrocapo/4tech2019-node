3c00543cc93c66f31e1816c126a84d22289b1c4e
Victor Nascimento <victor.nascimento@venturus.org.br>
2019-02-06 16:19:57 -0200

    Added cors

--- config/server.js
+++ config/server.js
@@ -9,11 +9,13 @@ const consign = require('consign')
 // ao 'body' das requisições
 const bodyParser = require('body-parser')
 // =================================================================
+const cors = require('cors')
 
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
+server.use(cors())
 
 consign()
     .include('./config/firebaseConfig.js')
